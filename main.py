38528d94eea709cc757293ca98575ad8b5f77d91
Voskal <andrei.daniel.danila@gmail.com>
2024-07-14 02:18:19 +0300

    updating main.py after blackout

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     word_no = count_words(text)
+    char_no = count_chars(text)
     print(text)
     print(f"This book has {word_no} words." )
+    print(text)
 
 def get_book_text(path):
     with open(path) as f:
@@ -13,4 +15,13 @@ def count_words(book):
     words = book.split()
     return len(words)
 
+def count_chars(book):
+    words = book.split()
+    char_count = {}
+    for char in words:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
 main()
